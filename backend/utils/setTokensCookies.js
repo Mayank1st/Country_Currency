6199a906c5aaa41b32892fd3012b1499cb61584e
Mayank Kumar <mayankdas99@gmail.com>
2024-09-25 02:59:31 +0530

    push_24092024

--- backend/utils/setTokensCookies.js
+++ backend/utils/setTokensCookies.js
@@ -1,9 +1,17 @@
-const setTokensCookies = (res, accessToken, refreshToken, newAccessTokenExp, newRefreshTokenExp) => {
-  const accessTokenMaxAge = (newAccessTokenExp - Math.floor(Date.now() / 1000)) * 1000;
-  const refreshTokenmaxAge = (newRefreshTokenExp - Math.floor(Date.now() / 1000)) * 1000;
+const setTokensCookies = (
+  res,
+  accessToken,
+  refreshToken,
+  newAccessTokenExp,
+  newRefreshTokenExp
+) => {
+  const accessTokenMaxAge =
+    (newAccessTokenExp - Math.floor(Date.now() / 1000)) * 1000;
+  const refreshTokenmaxAge =
+    (newRefreshTokenExp - Math.floor(Date.now() / 1000)) * 1000;
 
   // Set Cookie for Access Token
-  res.cookie('accessToken', accessToken, {
+  res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: true, // Set to true if using HTTPS
     maxAge: accessTokenMaxAge,
@@ -11,19 +19,20 @@ const setTokensCookies = (res, accessToken, refreshToken, newAccessTokenExp, new
   });
 
   // Set Cookie for Refresh Token
-  res.cookie('refreshToken', refreshToken, {
+  res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: true, // Set to true if using HTTPS
     maxAge: refreshTokenmaxAge,
     // sameSite: 'strict', // Adjust according to your requirements
   });
   // Set Cookie for is_auth
-  res.cookie('is_auth', true, {
-    httpOnly: false,
+  res.cookie("is_auth", true, {
+    httpOnly: true,
     secure: false, // Set to true if using HTTPS
     maxAge: refreshTokenmaxAge,
+    sameSite: "None",
     // sameSite: 'strict', // Adjust according to your requirements
   });
-}
+};
 
-export default setTokensCookies
+export default setTokensCookies;
